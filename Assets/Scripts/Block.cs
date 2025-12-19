d525c3f03afea305d1979e75736e7c84efac94dc
Gabriel Vasile <g.vaSile@gmail.com>
2025-12-19 10:49:09 -0800

    Implemented block health.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,9 +4,19 @@ namespace Breakout
 {
     public class Block : MonoBehaviour
     {
+        [SerializeField] private int health = 1;
+
+        private SpriteRenderer _spriteRenderer;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         private void Start()
         {
+            AutoSetSpriteRenderer();
+        }
+
+        private void AutoSetSpriteRenderer()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         // Update is called once per frame
@@ -16,9 +26,34 @@ namespace Breakout
 
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (other.gameObject.CompareTag("Ball"))
+            if (!other.gameObject.CompareTag("Ball"))
             {
-                Destroy(gameObject);
+                return;
+            }
+
+            health--;
+            switch (health)
+            {
+                case <= 0:
+                {
+                    Destroy(gameObject);
+                    break;
+                }
+                case 1:
+                {
+                    _spriteRenderer.color = Color.mediumSpringGreen;
+                    break;
+                }
+                case 2:
+                {
+                    _spriteRenderer.color = Color.softYellow;
+                    break;
+                }
+                case 3:
+                {
+                    _spriteRenderer.color = Color.red;
+                    break;
+                }
             }
         }
     }
