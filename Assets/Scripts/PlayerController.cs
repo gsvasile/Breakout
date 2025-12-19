1d2841ac4e1ba91b05396be60bf9e6fb497a7fdc
Gabriel Vasile <g.vaSile@gmail.com>
2025-12-19 01:54:55 -0800

    Added initial game board and player and player controller.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Breakout.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Movement")]
+        [SerializeField] private float speed = 2f;
+
+        [Header("Player Input")]
+        [SerializeField] private Key rightKey = Key.D;
+        [SerializeField] private Key leftKey = Key.A;
+
+        private Rigidbody2D _myRigidBody;
+        // Stores direction that the player will move
+        private float _direction;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Start()
+        {
+            AutoSetRigidBody();
+        }
+
+        private void AutoSetRigidBody()
+        {
+            if (_myRigidBody != null)
+            {
+                return;
+            }
+
+            var rigidBody = GetComponent<Rigidbody2D>();
+            if (rigidBody != null)
+            {
+                _myRigidBody = rigidBody;
+            }
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            PlayerInput();
+        }
+
+        private void FixedUpdate()
+        {
+            _myRigidBody.linearVelocity = new Vector2(_direction, 0) * speed;
+        }
+
+        private void PlayerInput()
+        {
+            var kb = Keyboard.current;
+            if (kb == null)
+            {
+                return;
+            }
+
+            if ((rightKey != Key.None && kb[rightKey].IsPressed()) ||
+                kb.rightArrowKey.IsPressed())
+            {
+                _direction = 1f;
+            }
+            else if ((leftKey != Key.None && kb[leftKey].IsPressed()) ||
+                     kb.leftArrowKey.IsPressed())
+            {
+                _direction = -1f;
+            }
+            else
+            {
+                _direction = 0f;
+            }
+        }
+    }
+}
